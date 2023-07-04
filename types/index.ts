9e606d42cfce35fb61b001f468c28a0b1ac53e26
fahimehsd <fahimehsadeghi3@gmail.com>
2023-07-04 22:29:41 +0330

    types added

---
+++ types/index.ts
@@ -0,0 +1,19 @@
+export interface NewsState {
+  id: number;
+  title: string;
+  desc: string;
+  image?: string;
+}
+export interface ProductsState {
+  id: number;
+  name: string;
+  desc: string;
+  image?: string;
+  price: number;
+}
+export interface MoreState {
+  id: number;
+  title: string;
+  desc: string;
+  image?: string;
+}
