325c1d8a22fd1bc6fda67e34e25c1d01485122c2
fahimehsd <fahimehsadeghi3@gmail.com>
2023-07-04 22:29:10 +0330

    constants added

---
+++ constants/index.ts
@@ -0,0 +1,93 @@
+export const HeaderLinks = [
+  {
+    id: 1,
+    title: "صفحه نخست",
+    path: "/"
+  },
+  {
+    id: 2,
+    title: "معرفی شرکت",
+    path: "/about"
+  },
+  {
+    id: 3,
+    title: "محصولات",
+    path: "/products"
+  },
+  {
+    id: 4,
+    title: "اخبار",
+    path: "/news"
+  },
+  {
+    id: 5,
+    title: "تماس با ما",
+    path: "/contact"
+  }
+];
+
+export const FooterLinks = [
+  {
+    id: 1,
+    title: "تماس با ما",
+    posts: [
+      {
+        id: 1,
+        title: "نشانی:",
+        desc: "تهران، خیابان ولیعصر بالاتر از میرداماد خیابان دستگردی پلاک ۳۶۴ طبقه ۳ واحد ۷ ، کدپستی: ۱۹۶۸۷۱۵۴۷۹",
+        href: "https://goo.gl/maps/A23WhtwWeLQwwuju7"
+      },
+
+      {
+        id: 2,
+        title: "تلفن:",
+        desc: "۰۲۱-۸۸۶۷۷۰۴۵",
+        href: "tel:02188677045"
+      },
+      {
+        id: 3,
+        title: "تلفن:",
+        desc: "۰۲۱-۸۸۶۷۷۰۴۵",
+        href: "tel:02188677045"
+      },
+      {
+        id: 4,
+        title: "کانال",
+        desc: "تلگرام",
+        href: "https://t.me/rohamsteel.com"
+      },
+      {
+        id: 5,
+        title: "پیج",
+        desc: "اینستاگرام",
+        href: "https://www.instagram.com/rohamsteel.com/"
+      },
+      {
+        id: 6,
+        title: "صفحه تخصصی",
+        desc: "لینکدین",
+        href: "https://www.linkedin.com/company/rohamsteel.com"
+      },
+
+      {
+        id: 7,
+        title: "ایمیل اطلاعات:",
+        desc: "info@rohamsteel.com",
+        href: "https://www.linkedin.com/company/rohamsteel.com"
+      }
+    ]
+  },
+  {
+    id: 2,
+    title: "درباره ما",
+    posts: [
+      {
+        id: 1,
+        title:
+          "شرکت رهام پارس جزو برندهای محبوب در بیش از ۵۰۰ سازمان، برگ افتخاری است که به پاس انتخاب اندیشمندانه مشتریان خود، به آن نائل آمده است.",
+        desc: "",
+        href: ""
+      }
+    ]
+  }
+];
