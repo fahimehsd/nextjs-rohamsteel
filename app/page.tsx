ae50371770bfdb3a93e4fcd49c4ac030b68be09e
fahimehsd <fahimehsadeghi3@gmail.com>
2023-07-05 22:24:16 +0330

    about page done

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ import React from "react";
 const Home = () => {
   return (
     <div className="bg-slate-100 flexCenter flex-col">
-      <aside>
+      <aside className="w-full">
         <Tags />
       </aside>
       <main>
