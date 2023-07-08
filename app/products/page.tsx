f5f40a13aa8ebc6dc19ee74cd52bbe20ee58443b
fahimehsd <fahimehsadeghi3@gmail.com>
2023-07-08 17:37:03 +0330

    products page responsive

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -7,6 +7,9 @@ import { BASE_URL } from "../api";
 import Link from "next/link";
 import Pagination from "@/components/Pagination";
 import ProductsCard from "@/components/ProductsCard";
+import NewsAside from "@/components/NewsAside";
+import CommentsAside from "@/components/CommentsAside";
+import MembershipAside from "@/components/MembershipAside";
 
 const Products = () => {
   const [products, setProducts] = useState<ProductsState[]>([]);
@@ -33,29 +36,36 @@ const Products = () => {
   const date = new Date().toLocaleDateString("fa-IR");
 
   return (
-    <div className="bg-gray-100 flexCenter flex-col py-4">
-      <div className="w-full mx-2">
-        <Tags />
-      </div>
-      <div className="flexBetween w-full p-2 mb-4">
-        <p className="font-bold text-xl">
-          محصولات شرکت <span className="text-red-500">رهام</span> پارس
-        </p>
-        <p>{date}</p>
-      </div>
-      <div className="bg-white w-full mx-2 px-2 py-4">
-        <ProductsCard data={currentPosts} />
-      </div>
-      <div className="w-full">
-        <Pagination
-          postsPerPage={postsPerPage}
-          totalPosts={products.length}
-          paginateFront={paginateFront}
-          paginateBack={paginateBack}
-          currentPage={currentPage}
-          paginate={paginate}
-        />
-      </div>
+    <div className="bg-gray-100 flexCenter md:items-start md:px-80 md:pt-5 md:gap-5 p-2">
+      <main className="flexCenter flex-col w-full md:w-2/3 md:gap-5">
+        <div className="w-full mx-2">
+          <Tags />
+        </div>
+        <div className="flexBetween w-full p-2 mb-4">
+          <p className="font-bold text-xl">
+            محصولات شرکت <span className="text-red-500">رهام</span> پارس
+          </p>
+          <p>{date}</p>
+        </div>
+        <div className="bg-white w-full mx-2 px-2 py-4">
+          <ProductsCard data={currentPosts} />
+        </div>
+        <div className="w-full">
+          <Pagination
+            postsPerPage={postsPerPage}
+            totalPosts={products.length}
+            paginateFront={paginateFront}
+            paginateBack={paginateBack}
+            currentPage={currentPage}
+            paginate={paginate}
+          />
+        </div>
+      </main>
+      <aside className="hidden md:flex md:flex-col md:gap-3 md:w-1/3">
+        <NewsAside />
+        <CommentsAside />
+        <MembershipAside />
+      </aside>
     </div>
   );
 };
